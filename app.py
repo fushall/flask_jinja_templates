73f30c2d6f902cb04c80bc591c21e80d96953ded
fushall <fushall@163.com>
2020-05-25 14:28:51 +0800

    demo for fetch api post method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def create_app():
@@ -13,6 +13,21 @@ def create_app():
     @app.route('/json', methods=['GET'])
     def get_json():
         return {'data': {'k': 'v'}}
+
+    @app.route('/web_page_title', methods=['POST'])
+    def get_web_page_title():
+        url = request.json.get('url')
+        try:
+            import re
+            import requests
+            response = requests.get(url)
+            match = re.search(r'<title>(?P<title>.+)</title>', response.text, re.MULTILINE)
+            if match:
+                return {'title': match.group('title')}
+            else:
+                return {'err': 'can not get the web page title.'}
+        except BaseException as e:
+            return {'err': str(e)}
 
     return app
 
